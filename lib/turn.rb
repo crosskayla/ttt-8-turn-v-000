b83681be67260fb416cf813d1e45aa673ae525d2
Kayla Cross <kac288@georgetown.edu>
2018-01-30 01:45:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  until valid_move?(board, index)
+  while ! valid_move?(board, index)
     puts "That's not a valid move. Please try again."
     input = gets.strip
   end
